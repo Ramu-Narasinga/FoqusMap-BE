779d57f6a415e58a020e348d12d6528f91e555a3
Ramu Narasinga <ramu199611@gmail.com>
2021-07-21 17:22:48 +0530

    updated port number

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ import helmet from 'helmet';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
